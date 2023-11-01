6f64aced92bea999ba40ea3f4857a2ddebbbc68f
Ido <Idosabach11@gmail.com>
2023-11-01 15:49:38 +0200

    Prototype practice

--- new.js
+++ new.js
@@ -42,6 +42,8 @@ player2.sayName() // logs 'also steve'
 // console.log(theHobbit.info());
 Object.getPrototypeOf(player1) === Player.prototype // returns true
 Object.getPrototypeOf(player2) === Player.prototype // returns true
+console.log(Object.getPrototypeOf(player1));
+console.log(Player.prototype);
 Player.prototype.sayHello = function() {
   console.log("Hello, I'm a player!");
 }
@@ -52,3 +54,26 @@ player2.sayHello() // logs "Hello, I'm a player!"
 // Don't do this!
 player1.__proto__ === Player.prototype // returns true
 player2.__proto__ === Player.prototype // returns true
+
+
+function Books(id,name,pages,year){
+  this.id = id
+  this.name =name
+  this.pages = pages
+  this.year = year
+}
+
+Books.prototype.info = function(){
+  console.log("info: " + this.id + " " + this.name + " " + this.pages + ' ' + this.year)
+}
+
+const Book1 = new Books(1,'ido',300,2000)
+const Book2 = new Books(2,'idoDo',100,2023)
+const arr = [Book1,Book2]
+
+// console.log(arr.length)
+
+for(let i = 0;i<arr.length;i++){
+  // console.log(arr.length)
+  console.log(arr[i].info())
+}
