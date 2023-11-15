292831f2f8035e827205c42e32ead8af9e143d06
Ido <Idosabach11@gmail.com>
2023-11-16 01:52:23 +0200

    test for tic tac toe project

--- dom.js
+++ dom.js
@@ -24,34 +24,147 @@ const pubsub = {
   },
 };
 
+function setGame() {
+  const arrOfBoard = Array(9).fill(null);
+  pubsub.publish("arrOfBoard",arrOfBoard)
 
-const dom = (function() {
-  const count = document.querySelector(".counter");
-  const btn = document.querySelector(".btn");
+  const player = {
+    name: "player",
+    marker: "x",
+    moves: [],
+  };
 
-  
-  btn.addEventListener('click', function() {
-    main.addCounter();
+  const computer = {
+    name: "root",
+    marker: "o",
+    moves: [],
+  };
+
+  let curr = player;
+
+  pubsub.subscribe("insertMarker" ,gameFlow)
+
+  function gameFlow(index) {
+    if (checkIfEmpty(index)) {
+      curr.moves.push(index);
+      console.log(curr.moves);
+      updateBoxContent(index);
+    }
+
+    if (finishGame(curr.moves)) {
+      if (curr.name === player.name) {
+        alert(`${curr.name} win!!!`)
+      } else if (curr.name === computer.name) {
+        alert(`${curr.name} lose!!!`)
+      }
+      restartGame();
+    } else if (player.moves.length + computer.moves.length === 9) {
+      pubsub.publish("restartBtn")
+      pubsub.subscribe("resetBoard",restartGame)
+    } else {
+      switchPlayer()
+    }
+  }
+
+  function updateBoxContent(index) {
+    const boxToUpdate = document.querySelector(`.box[data-num="${index}"]`);
+    boxToUpdate.textContent = curr.marker;
+  }
+
+  function switchPlayer() {
+    if (curr === player) {
+      curr = computer;
+    } else {
+      curr = player;
+    }
+  }
+
+  pubsub.subscribe("checkIfEmpty",checkIfEmpty)
+  function checkIfEmpty(index) {
+    if (arrOfBoard[index] === null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const winningMovies = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  function finishGame(arrToCheck) {
+    return winningMovies.some((winningCombo) =>
+      winningCombo.every((position) => arrToCheck.includes(position))
+    );
+  }
+
+  function restartGame() {
+    player.moves = [];
+    computer.moves = [];
+    curr = player;
+    arrOfBoard.fill(null);
+    pubsub.publish("cleanBoard")
+  }
+}
+
+
+function dom() {
+
+  const yourName = document.querySelector(".name-player");
+  const popup = document.querySelector(".popup-start");
+  const input = document.querySelector(".input-of-popup");
+  const btn = document.querySelector(".startGame");
+  const box = document.querySelectorAll(".box");
+  const restartBtn = document.querySelector(".restart-btn"); 
+
+  pubsub.subscribe("restartBtn",restartBtnChange)
+  function restartBtnChange(){
+    restartBtn.style.display = 'flex';
+  }
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nameUser = input.value;
+    if (nameUser === "") {
+      alert("Enter your name!");
+    } else {
+      popup.style.display = "none";
+      yourName.textContent = `player: ${nameUser}`;
+    }
   });
 
-  return {
-    count,
-    btn,
-  };
-})();
+  restartBtn.addEventListener('click',()=>{
+    pubsub.publish("resetBoard")
+    restartBtn.style.display = 'none';
+  })
 
+  box.forEach((btnOdBoard) => {
+    btnOdBoard.addEventListener("click", function (e) {
+      
+      let num = e.target.dataset.num;
+      pubsub.publish("insertMarker",num)
+      if (pubsub.publish("checkIfEmpty")) {
+        btnOdBoard.textContent = curr.marker;
+      }
+    });
+  });
+
+  pubsub.subscribe("cleanBoard",cleanBoard)
+  function cleanBoard() {
+    box.forEach((btn) => {
+      btn.textContent = "";
+    });
+  }
+}
 
 const main = (function(){
-  let sum = 0
-  function addCounter(){
-    sum += 1 ;
-    console.log(sum)
-    dom.count.textContent = sum
-  }
-
-  return {
-    sum,
-    addCounter,
-  }
+  dom()
+  setGame()
 })()
-
