a98e62a0b852cbe3550bf162d87f27cece63b85c
Ido <Idosabach11@gmail.com>
2023-11-20 21:16:40 +0200

    new project of learn webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 import joke from "./javascriptJoke";
+import myName, { functionOne, functionTwo } from "./myName";
 
 console.log(joke())
+
+function component(){
+  const element = document.createElement('div')
+
+  element.textContent = myName('code')
+  return element
+}
+
+document.body.appendChild(component())
+
+console.log(functionOne())
+console.log(functionTwo())
+
+
---
+++ src/myName.js
@@ -0,0 +1,12 @@
+const myName = (name) => 'hi! my name is' + " " + name;
+
+export default myName
+
+// a file called myModule.js
+const functionOne = () => 'ONE';
+const functionTwo = () => 'TWO';
+
+export {
+  functionOne,
+  functionTwo
+};
