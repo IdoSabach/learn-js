9431095ca004ced30b000f5d286856d38156c22d
Ido <Idosabach11@gmail.com>
2023-12-27 02:07:12 +0200

    anima

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("// const email = document.getElementById(\"email\");\r\n\r\n// email.addEventListener(\"input\", (event) => {\r\n//   if (email.validity.typeMismatch) {\r\n//     email.setCustomValidity(\"I am expecting an email address!\");\r\n//   } else {\r\n//     email.setCustomValidity(\"\");\r\n//   }\r\n// });\r\nconst city = 'Israel';\r\nconst apiKey = 'd63972b3d5c94796b4f164912231012';\r\nconst apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&units=metric&appid=${apiKey}`;\r\n\r\nconst getDate = async () => {\r\n  try {\r\n    const response = await fetch(apiUrl);\r\n\r\n    if (!response.ok) {\r\n      throw new Error(`HTTP error! Status: ${response.status}`);\r\n    }\r\n\r\n    const data = await response.json();\r\n    console.log(data);\r\n  } catch (error) {\r\n    console.log('Error fetching data:', error.message);\r\n  }\r\n};\r\n\r\ngetDate();\r\n\r\n\n\n//# sourceURL=webpack://learn-js/./src/index.js?");
+eval("\n\n//# sourceURL=webpack://learn-js/./src/index.js?");
 
 /***/ })
 
